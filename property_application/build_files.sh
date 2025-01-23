d1a2d98bdf53c2c1b1741f2c74f576b4fb2e9bab
ram <peswani.ram.33@gmail.com>
2025-01-24 02:20:45 +0530

    dfdfd

--- property_application/build_files.sh
+++ property_application/build_files.sh
@@ -1,4 +1,14 @@
 echo "BUILD START"
- python3.12.2 -m pip install -r requirements.txt
- python3.12.2 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+
+# Update package manager and install PostgreSQL development libraries
+apt-get update && apt-get install -y libpq-dev gcc
+
+# Ensure pip and dependencies are installed
+python3.12 -m ensurepip --upgrade
+python3.12 -m pip install --upgrade pip
+python3.12 -m pip install -r requirements.txt
+
+# Collect static files
+python3.12 manage.py collectstatic --noinput --clear
+
+echo "BUILD END"
